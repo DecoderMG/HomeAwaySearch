06e83392c9239b475d31dc87838aa53c19be2752
Dakota Gallimore <gallimoredakota@gmail.com>
2017-11-03 14:17:30 -0400

    Add Test case for Amenities Json Parsing

--- app/src/test/java/com/dakota/gallimore/homeawaysearch/ExampleUnitTest.java
+++ app/src/test/java/com/dakota/gallimore/homeawaysearch/ExampleUnitTest.java
@@ -1,5 +1,9 @@
 package com.dakota.gallimore.homeawaysearch;
 
+import com.dakota.gallimore.homeawaysearch.DataClasses.Amenities;
+import com.dakota.gallimore.homeawaysearch.Utils.JsonUtils;
+
+import org.json.JSONObject;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -13,5 +17,16 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void JsonAminitiesTest() throws Exception {
+        String aminityJson = "{\"count\": 1,\"category\": \"AMENITY\",\"description\": \"Shower\",\"localizedName\": \"shower\"}";
+        JSONObject jsonObject = new JSONObject(aminityJson);
+        Amenities amenities = JsonUtils.parseAmenityJson(jsonObject);
+        assertEquals(1, amenities.getCount());
+        assertEquals("AMENITY", amenities.getCategory());
+        assertEquals("Shower", amenities.getDescription());
+        assertEquals("shower", "shower");
     }
 }
