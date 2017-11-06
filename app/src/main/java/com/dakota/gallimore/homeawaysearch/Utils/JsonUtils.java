ab77db7df1d918bb4fe18563e4eefd7a02e22bf2
Dakota Gallimore <gallimoredakota@gmail.com>
2017-11-06 02:11:40 -0500

    Add ListingMedia json parsing

--- app/src/main/java/com/dakota/gallimore/homeawaysearch/Utils/JsonUtils.java
+++ app/src/main/java/com/dakota/gallimore/homeawaysearch/Utils/JsonUtils.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import com.dakota.gallimore.homeawaysearch.DataClasses.Amenities;
 import com.dakota.gallimore.homeawaysearch.DataClasses.Feature;
+import com.dakota.gallimore.homeawaysearch.DataClasses.ListingMedia;
 import com.dakota.gallimore.homeawaysearch.DataClasses.RatePeriod;
 import com.dakota.gallimore.homeawaysearch.DataClasses.Review;
 import com.dakota.gallimore.homeawaysearch.DataClasses.Room;
@@ -14,6 +15,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -241,6 +244,36 @@ public class JsonUtils {
         } catch (JSONException e) {
             Log.d("JSON Utils: ", e.getMessage());
             throw new JSONException("Invalid JSON data for User type");
+        }
+    }
+
+    public static ListingMedia parseMediaJson(JSONObject jsonObject, String imageType) throws JSONException, MalformedURLException {
+        String caption = "";
+        int height = 0;
+        int width = 0;
+        URL url = null;
+        int unitNumber = 0;
+
+        try {
+            if (!imageType.equals("photo")) {
+                caption = jsonObject.getJSONObject("photo").getString("caption");
+                unitNumber = jsonObject.getInt("unitNumber");
+            } else {
+                caption = jsonObject.getString("caption");
+            }
+            JSONObject imageSpecs = jsonObject.getJSONObject("large");
+            height = imageSpecs.getJSONObject("dimension").getInt("height");
+            width = imageSpecs.getJSONObject("dimension").getInt("width");
+            url = new URL(imageSpecs.getString("uri"));
+
+            return new ListingMedia(caption, height, width, imageType, url, unitNumber);
+
+        } catch (JSONException e) {
+            Log.d("JSON Utils: ", e.getMessage());
+            throw new JSONException("Invalid JSON data for Media type");
+        } catch (MalformedURLException e) {
+            Log.d("JSON Utils: ", e.getMessage());
+            throw new JSONException("Unable to parse image URI");
         }
     }
 }
