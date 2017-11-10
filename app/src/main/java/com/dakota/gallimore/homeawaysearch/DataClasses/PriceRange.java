876493f86f7b423d632171d72a3e8dc93baebf92
Dakota Gallimore <gallimoredakota@gmail.com>
2017-11-10 15:22:24 -0500

    Add Price Range Class

---
+++ app/src/main/java/com/dakota/gallimore/homeawaysearch/DataClasses/PriceRange.java
@@ -0,0 +1,55 @@
+package com.dakota.gallimore.homeawaysearch.DataClasses;
+
+/**
+ * Created by galli_000 on 11/10/2017.
+ */
+
+public class PriceRange {
+
+    private double to;
+    private String currencyUnits;
+    private String periodType;
+    private double from;
+
+    public PriceRange() {
+    }
+
+    public PriceRange(double to, String currencyUnits, String periodType, double from) {
+        this.to = to;
+        this.currencyUnits = currencyUnits;
+        this.periodType = periodType;
+        this.from = from;
+    }
+
+    public double getToPrice() {
+        return to;
+    }
+
+    public void setToPrice(double to) {
+        this.to = to;
+    }
+
+    public String getCurrencyUnits() {
+        return currencyUnits;
+    }
+
+    public void setCurrencyUnits(String currencyUnits) {
+        this.currencyUnits = currencyUnits;
+    }
+
+    public String getPeriodType() {
+        return periodType;
+    }
+
+    public void setPeriodType(String periodType) {
+        this.periodType = periodType;
+    }
+
+    public double getFromPrice() {
+        return from;
+    }
+
+    public void setFromPrice(double from) {
+        this.from = from;
+    }
+}
