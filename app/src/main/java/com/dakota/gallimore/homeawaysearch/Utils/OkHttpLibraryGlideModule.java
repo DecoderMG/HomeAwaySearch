3d5318fcfed404fe3e23152f7ea3ba18ec677821
Dakota Gallimore <gallimoredakota@gmail.com>
2017-12-18 20:47:06 -0500

    Fix GlideApp OkHttp NullPointerException

--- app/src/main/java/com/dakota/gallimore/homeawaysearch/Utils/OkHttpLibraryGlideModule.java
+++ app/src/main/java/com/dakota/gallimore/homeawaysearch/Utils/OkHttpLibraryGlideModule.java
@@ -8,17 +8,27 @@ import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
 import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.AppGlideModule;
+import com.dakota.gallimore.homeawaysearch.di.HomeAwaySearchApplication;
 
 import java.io.InputStream;
+
+import javax.inject.Inject;
+
+import okhttp3.OkHttpClient;
 
 /**
  * Created by galli_000 on 11/10/2017.
  */
 @GlideModule
 public final class OkHttpLibraryGlideModule extends AppGlideModule {
+
+    @Inject
+    OkHttpClient client;
+
     @Override
     public void registerComponents(Context context, Glide glide, Registry registry) {
-        OkHttpUrlLoader.Factory factory = new OkHttpUrlLoader.Factory(NetworkUtils.getNewHttpClient());
+        HomeAwaySearchApplication.get(context).getAppComponent().inject(this);
+        OkHttpUrlLoader.Factory factory = new OkHttpUrlLoader.Factory(client);
         glide.getRegistry().replace(GlideUrl.class, InputStream.class, factory);
     }
 }
