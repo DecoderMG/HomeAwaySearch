b545878c749822fbab0a0fd89d476007063ed124
Dakota Gallimore <gallimoredakota@gmail.com>
2017-12-18 20:40:03 -0500

    Update NetworkUtils getHomeAwayJsonData to accept OkHttpClient.

--- app/src/main/java/com/dakota/gallimore/homeawaysearch/Utils/NetworkUtils.java
+++ app/src/main/java/com/dakota/gallimore/homeawaysearch/Utils/NetworkUtils.java
@@ -1,24 +1,16 @@
 package com.dakota.gallimore.homeawaysearch.Utils;
 
-import android.os.Build;
 import android.util.Log;
 
 import com.dakota.gallimore.homeawaysearch.Constants;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import javax.net.ssl.SSLContext;
 
 import okhttp3.Call;
 import okhttp3.Callback;
-import okhttp3.ConnectionSpec;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import okhttp3.TlsVersion;
 
 /**
  * Created by galli_000 on 11/1/2017.
@@ -34,11 +26,7 @@ public class NetworkUtils {
      * @param accessToken     - accessToken required to access data
      * @param networkCallback - Callback to monitor when asynchronous data is returned
      */
-    public static void getHomeAwayJsonData(String url, String accessToken, final NetworkCallback networkCallback) {
-
-        // use the access token to do something ...
-        OkHttpClient client = new OkHttpClient.Builder()
-                .build();
+    public static void getHomeAwayJsonData(String url, String accessToken, OkHttpClient client, final NetworkCallback networkCallback) {
 
         Request request = new Request.Builder()
                 .url(url)
@@ -69,29 +57,25 @@ public class NetworkUtils {
             }
         });
     }
-
+/*
     public static OkHttpClient.Builder enableTls12OnPreLollipop(OkHttpClient.Builder client) {
         if (Build.VERSION.SDK_INT >= 16 && Build.VERSION.SDK_INT < 22) {
             try {
                 SSLContext sc = SSLContext.getInstance("TLSv1.2");
                 sc.init(null, null, null);
                 client.sslSocketFactory(new Tls12SocketFactory(sc.getSocketFactory()));
-
                 ConnectionSpec cs = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
                         .tlsVersions(TlsVersion.TLS_1_2)
                         .build();
-
                 List specs = new ArrayList<>();
                 specs.add(cs);
                 specs.add(ConnectionSpec.COMPATIBLE_TLS);
                 specs.add(ConnectionSpec.CLEARTEXT);
-
                 client.connectionSpecs(specs);
             } catch (Exception exc) {
                 Log.e("OkHttpTLSCompat", "Error while setting TLS 1.2", exc);
             }
         }
-
         return client;
     }
 
@@ -104,5 +88,5 @@ public class NetworkUtils {
                 .writeTimeout(5, TimeUnit.SECONDS)
                 .readTimeout(5, TimeUnit.SECONDS);
         return enableTls12OnPreLollipop(client).build();
-    }
+    } */
 }
